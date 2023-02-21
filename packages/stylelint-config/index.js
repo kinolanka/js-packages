9c91133ae56eae4f6ca97dece8defa7d1ca4f99e
Volodymyr <kinolanka@gmail.com>
2023-02-21 17:01:09 +0200

    stylelint-config@0.0.1

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,5 @@
+throw new Error(
+  'Please update your styleLint configuration to import one of the' +
+    ' profile paths such as "@kinolanka/stylelint-config/profile/<profile-name>"' +
+    '\n\nSee the documentation for details: https://www.npmjs.com/package/@kinolanka/stylelint-config'
+);
