0220d986416852bd33419f9cf1bba5ccee1ef6e4
Volodymyr <kinolanka@gmail.com>
2023-02-21 17:02:03 +0200

    Small fix

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,5 @@
 throw new Error(
   'Please update your ESLint configuration to import one of the' +
-    ' profile paths such as "@kinolanka/eslint-config/profile/next"' +
+    ' profile paths such as "@kinolanka/eslint-config/profile/<profile-name>"' +
     '\n\nSee the documentation for details: https://www.npmjs.com/package/@kinolanka/eslint-config'
 );
